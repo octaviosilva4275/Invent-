cab7ce329198428f7e0a294de88b2804ad356e39
Octavio Augusto <octavio.freitas4275@gmail.com>
2024-09-19 14:38:27 -0300

    conectar banco de dados 4g

--- app.py
+++ app.py
@@ -3,9 +3,9 @@ import mysql.connector
 from mysql.connector import Error
 import os
 from twilio.rest import Client
-# from dotenv import load_dotenv
+from dotenv import load_dotenv
 
-# load_dotenv()  # Carregar variáveis do .env
+load_dotenv()  # Carregar variáveis do .env
 
 app = Flask(__name__)
 
@@ -28,51 +28,51 @@ app.secret_key = 'teste'
 #     body=f'Mensagem enviada')
 # print(message.sid)
 
-# def verificar_tabela():
-#     try:
-#         conexao = mysql.connector.connect(
-#             host=os.getenv('MYSQL_HOST'),
-#             user=os.getenv('MYSQL_USER'),
-#             password=os.getenv('MYSQL_PASSWORD'),
-#             database=os.getenv('MYSQL_DB'),
-#             port=int(os.getenv('MYSQL_PORT'))
-#         )
-#         cursor = conexao.cursor()
-#         cursor.execute("SHOW TABLES")
-#         tables = cursor.fetchall()
-#         print("Tabelas no banco de dados:")
-#         for table in tables:
-#             print(table[0])
-#         cursor.close()
-#         conexao.close()
-#     except mysql.connector.Error as err:
-#         print(f"Erro: {err}")
+def verificar_tabela():
+    try:
+        conexao = mysql.connector.connect(
+            host=os.getenv('MYSQL_HOST'),
+            user=os.getenv('MYSQL_USER'),
+            password=os.getenv('MYSQL_PASSWORD'),
+            database=os.getenv('MYSQL_DB'),
+            port=int(os.getenv('MYSQL_PORT'))
+        )
+        cursor = conexao.cursor()
+        cursor.execute("SHOW TABLES")
+        tables = cursor.fetchall()
+        print("Tabelas no banco de dados:")
+        for table in tables:
+            print(table[0])
+        cursor.close()
+        conexao.close()
+    except mysql.connector.Error as err:
+        print(f"Erro: {err}")
 
-# verificar_tabela()
+verificar_tabela()
 
 def conectar_banco_dados():
-    # use_remote_db = os.getenv('USE_REMOTE_DB', 'False').lower() == 'true'
-    # print(f"USE_REMOTE_DB: {use_remote_db}")
+    use_remote_db = os.getenv('USE_REMOTE_DB', 'False').lower() == 'true'
+    print(f"USE_REMOTE_DB: {use_remote_db}")
     
     try:
-        # if use_remote_db:
-        #     print("Tentando conectar ao banco de dados remoto...")
-        #     conexao = mysql.connector.connect(
-        #         host=os.getenv('MYSQL_HOST'),
-        #         user=os.getenv('MYSQL_USER'),
-        #         password=os.getenv('MYSQL_PASSWORD'),
-        #         database=os.getenv('MYSQL_DB'),
-        #         port=int(os.getenv('MYSQL_PORT'))
-        #     )
-        # else:
-        print("Tentando conectar ao banco de dados local...")
-        conexao = mysql.connector.connect(
-            host='localhost',
-            user='tcc',
-            password='123',
-            database='almoxarifado',
-        )
-        print("Conexão estabelecida com sucesso!")
+        if use_remote_db:
+            print("Tentando conectar ao banco de dados remoto...")
+            conexao = mysql.connector.connect(
+                host=os.getenv('MYSQL_HOST'),
+                user=os.getenv('MYSQL_USER'),
+                password=os.getenv('MYSQL_PASSWORD'),
+                database=os.getenv('MYSQL_DB'),
+                port=int(os.getenv('MYSQL_PORT'))
+            )
+        else:
+            print("Tentando conectar ao banco de dados local...")
+            conexao = mysql.connector.connect(
+                host='localhost',
+                user='tcc',
+                password='123',
+                database='almoxarifado',
+            )
+            print("Conexão estabelecida com sucesso!")
         return conexao
     except Error as e:
         print(f"Erro ao conectar ao banco de dados: {e}")
