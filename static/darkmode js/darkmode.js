58ec1807eb86983fccd774c8ab406e0fe017d37c
Octavio Augusto <octavio.freitas4275@gmail.com>
2024-12-08 18:43:38 -0300

    modo escuro mobile

--- static/darkmode js/darkmode.js
+++ static/darkmode js/darkmode.js
@@ -1,6 +1,10 @@
-document.addEventListener('DOMContentLoaded', function() {
+document.addEventListener('DOMContentLoaded', function () {
     const luaIcon = document.getElementById('lua-icon');
     const solIcon = document.getElementById('sol-icon');
+    const luaIconMobile = document.querySelector('.lua-icon');
+    const solIconMobile = document.querySelector('.sol-icon');
+    const darkModeDesktop = document.querySelector('.darkmode'); // Pai do desktop
+    const darkModeMobile = document.querySelector('#modo-escuro-mobile'); // Pai do mobile
     const body = document.body;
 
     // Verificar a preferência do tema no localStorage
@@ -15,6 +19,7 @@ document.addEventListener('DOMContentLoaded', function() {
         body.classList.remove('light-mode');
         body.classList.add('dark-mode');
         toggleIcons(luaIcon, solIcon);
+        toggleIcons(luaIconMobile, solIconMobile);
         localStorage.setItem('theme', 'dark'); // Salva a preferência no localStorage
     }
 
@@ -22,15 +27,32 @@ document.addEventListener('DOMContentLoaded', function() {
         body.classList.remove('dark-mode');
         body.classList.add('light-mode');
         toggleIcons(solIcon, luaIcon);
+        toggleIcons(solIconMobile, luaIconMobile);
         localStorage.setItem('theme', 'light'); // Salva a preferência no localStorage
     }
 
     function toggleIcons(hiddenIcon, visibleIcon) {
-        hiddenIcon.classList.add('none');
-        visibleIcon.classList.remove('none');
+        if (hiddenIcon && visibleIcon) {
+            hiddenIcon.classList.add('none');
+            visibleIcon.classList.remove('none');
+        }
     }
 
-    // Eventos de clique para os ícones
-    luaIcon.addEventListener('click', activateDarkMode);
-    solIcon.addEventListener('click', activateLightMode);
+    // Adicionar eventos para desktop (clique no span ou nas imagens)
+    darkModeDesktop.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
+            activateLightMode();
+        } else {
+            activateDarkMode();
+        }
+    });
+
+    // Adicionar eventos para mobile (clique no span ou nas imagens)
+    darkModeMobile.addEventListener('click', () => {
+        if (body.classList.contains('dark-mode')) {
+            activateLightMode();
+        } else {
+            activateDarkMode();
+        }
+    });
 });
